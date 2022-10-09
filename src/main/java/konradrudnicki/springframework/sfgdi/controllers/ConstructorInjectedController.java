b7d7fcf779fa6f4b6326cbcd628df31da1e24484
KonradRudnicki <97458824+KonradRudnicki@users.noreply.github.com>
2022-10-10 00:48:54 +0200

    Dependency Injection using Spring

--- src/main/java/konradrudnicki/springframework/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/konradrudnicki/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -1,7 +1,9 @@
 package konradrudnicki.springframework.sfgdi.controllers;
 
 import konradrudnicki.springframework.sfgdi.services.GreetingService;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
     private final GreetingService greetingService;
 
--- src/main/java/konradrudnicki/springframework/sfgdi/controllers/PropertyInjectedController.java
+++ src/main/java/konradrudnicki/springframework/sfgdi/controllers/PropertyInjectedController.java
@@ -1,9 +1,13 @@
 package konradrudnicki.springframework.sfgdi.controllers;
 
 import konradrudnicki.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class PropertyInjectedController {
 
+    @Autowired
     public GreetingService greetingService;
 
     public String getGreeting(){
