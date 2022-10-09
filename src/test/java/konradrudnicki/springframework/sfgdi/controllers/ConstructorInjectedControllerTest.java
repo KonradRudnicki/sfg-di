6360636120e053773ec929477cbf78ea87859bb2
KonradRudnicki <97458824+KonradRudnicki@users.noreply.github.com>
2022-10-10 01:26:04 +0200

    Qualifiers

--- src/test/java/konradrudnicki/springframework/sfgdi/controllers/ConstructorInjectedControllerTest.java
+++ src/test/java/konradrudnicki/springframework/sfgdi/controllers/ConstructorInjectedControllerTest.java
@@ -1,10 +1,8 @@
 package konradrudnicki.springframework.sfgdi.controllers;
 
-import konradrudnicki.springframework.sfgdi.services.GreetingServiceImpl;
+import konradrudnicki.springframework.sfgdi.services.ConstructorGreetingService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class ConstructorInjectedControllerTest {
     ConstructorInjectedController controller;
@@ -12,7 +10,7 @@ class ConstructorInjectedControllerTest {
     @BeforeEach
     void setUp() {
 
-        controller = new ConstructorInjectedController(new GreetingServiceImpl());
+        controller = new ConstructorInjectedController(new ConstructorGreetingService());
     }
 
     @Test
