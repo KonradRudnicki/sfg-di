ff4fbf02f8452a700ecb180c68d8b0ef385c55d5
konradrudnicki <konradrudnicki0@gmail.com>
2022-11-15 22:35:53 +0100

    GreetingServiceConfig - adding primary beans

--- src/main/java/guru/springframework/sfgdi/services/PrimaryGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/PrimaryGreetingService.java
@@ -6,10 +6,7 @@ import org.springframework.stereotype.Service;
 /**
  * Created by jt on 12/27/19.
  */
-@Primary
-@Service
 public class PrimaryGreetingService implements GreetingService {
-
     @Override
     public String sayGreeting() {
         return "Hello World - From the PRIMARY Bean";
